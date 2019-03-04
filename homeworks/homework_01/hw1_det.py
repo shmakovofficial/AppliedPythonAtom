cead287cc1270c4ed05437a22388c0f504449b08
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-03-04 20:42:03 +0300

    Fix 8

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,6 +3,29 @@
 
 
 def calculate_determinant(list_of_lists):
+    for i in list_of_lists:
+        if len(i) is not len(list_of_lists):
+            return None
+
+    def add_matrix(matrix, el_x, el_y):
+        new_matrix = [j.copy() for j in matrix]
+        for j in new_matrix:
+            j.pop(el_y)
+        new_matrix.pop(el_x)
+        return new_matrix
+
+    def calc_det(matrix):
+        if len(matrix) < 2:
+            return 1
+        else:
+            result = 0
+            for j in range(len(matrix)):
+                result += matrix[0][j] * calc_det(add_matrix(matrix, 0, j))
+            return result
+
+    if len(list_of_lists) is 1:
+        return list_of_lists[0][0]
+    return calc_det(list_of_lists)
     '''
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
