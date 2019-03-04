859c671871171f165d55aab4291545289062e60a
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-03-04 20:54:14 +0300

    Fix 9

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -8,23 +8,27 @@ def calculate_determinant(list_of_lists):
             return None
 
     def add_matrix(matrix, el_x, el_y):
+        print("Creating additional matrix from: ", matrix, " without el from: ", el_x, "::", el_y)
         new_matrix = [j.copy() for j in matrix]
         for j in new_matrix:
             j.pop(el_y)
         new_matrix.pop(el_x)
+        print("Result is: ", new_matrix)
         return new_matrix
 
     def calc_det(matrix):
         if len(matrix) < 2:
-            return 1
+            return matrix[0][0]
         else:
             result = 0
+            sign = -1
             for j in range(len(matrix)):
-                result += matrix[0][j] * calc_det(add_matrix(matrix, 0, j))
+                sign *= -1
+                print("The sign is now: ", sign)
+                result += matrix[0][j] * calc_det(add_matrix(matrix, 0, j)) * sign
+                print("Result changed to: ", result)
             return result
 
-    if len(list_of_lists) is 1:
-        return list_of_lists[0][0]
     return calc_det(list_of_lists)
     '''
     Метод, считающий детерминант входной матрицы,
@@ -34,4 +38,3 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
