910722d06e59aadd2e579a899e074b7419241158
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-03-08 23:01:31 +0300

    Heap done

--- homeworks/homework_02/fastmerger.py
+++ homeworks/homework_02/fastmerger.py
@@ -12,4 +12,12 @@ class FastSortedListMerger:
         принимает на вход список отсортированных непоубыванию списков и число
         на выходе выдает один список длинной k, отсортированных по убыванию
         '''
-        raise NotImplementedError
+        merged_list = list()
+        for i in list_of_lists:
+            merged_list += i
+        merged_list = list(map(lambda x: (x, x), merged_list))
+        h = MaxHeap(merged_list)
+        result = list()
+        for i in range(k):
+            result.append(h.extract_maximum()[0])
+        return result
--- homeworks/homework_02/heap.py
+++ homeworks/homework_02/heap.py
@@ -2,25 +2,51 @@
 # coding: utf-8
 
 
-class Heap():
+class Heap:
 
     def __init__(self, array):
-        pass
+        self.data = array[:]
+        self.build_heap()
 
     def add(self, elem_with_priority):
-        pass
+        self.data.append(elem_with_priority)
+        self.build_heap()
 
     def build_heap(self):
-        pass
+        if len(self.data):
+            for i in reversed(range(len(self.data) // 2)):
+                self._sift_down(i)
+
+    def _sift_down(self, index: int):
+        max_index = self._get_max_index(index, index * 2 + 1)
+        max_index = self._get_max_index(max_index, index * 2 + 2)
+        if max_index != index:
+            self.data[index], self.data[max_index] = \
+                self.data[max_index], self.data[index]
+            self._sift_down(max_index)
+
+    def _get_max_index(self, i: int, j: int):
+        if i < len(self.data):
+            if j < len(self.data):
+                return i if comparator_d(self.data[i], self.data[j]) else j
+            else:
+                return i
+        else:
+            if j < len(self.data):
+                return j
+            else:
+                return None
 
 
 class MaxHeap(Heap):
 
     def __init__(self, array):
-        raise NotImplementedError
+        super().__init__(array)
 
     def extract_maximum(self):
-        pass
+        max_element = self.data.pop(0)
+        self.build_heap()
+        return max_element
 
 
 def comparator_d(x, y):
@@ -30,3 +56,13 @@ def comparator_d(x, y):
         return True
     else:
         return False
+
+
+'''
+import pickle
+
+with open("../../tests/tests_data/test_hw_02_fastmerger.ini.pkl", "rb") as f:
+    data = pickle.load(f)
+    for i in data:
+        print(i)
+'''
