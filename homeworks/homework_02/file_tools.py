12fe90898c98524bd5ec101024f09eaf4a2eb844
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-03-10 23:33:38 +0300

    Table printing modification

--- homeworks/homework_02/file_tools.py
+++ homeworks/homework_02/file_tools.py
@@ -26,26 +26,29 @@ def _parse_to_list(filename, file_charset, file_format):
 
 
 def printing(filename, file_charset, file_format, headers=True):
-
     list_of_lists = _parse_to_list(filename, file_charset, file_format)
-    if len(list_of_lists) - int(headers) < 1:
-        print("File is empty")
-    else:
-        lengths = list()
-        for i in range(len(list_of_lists[0])):
-            current_length = 0
-            for j in list_of_lists:
-                current_length = max(len(str(j[i])), current_length)
-            lengths.append(current_length)
-        print("-" * (sum(lengths) + (len(lengths) + 1) * 3))
-        if headers:
-            data_headers = list_of_lists.pop(0)
-            for i, j in enumerate(data_headers):
-                print(" | {:^{width}}".format(j, width=lengths[i]), end='')
-            print(" | ")
-            print("-" * (sum(lengths) + (len(lengths) + 1) * 3))
-        for i in list_of_lists:
-            for j, k in enumerate(i):
-                print(" | {:<{width}}".format(k, width=lengths[j]), end='')
-            print(" | ")
-        print("-" * (sum(lengths) + (len(lengths) + 1) * 3))
+    if len(list_of_lists) < 1:
+        print("Формат не валиден")
+        return None
+    for i in list_of_lists:
+        if len(i) != len(list_of_lists[0]):
+            print("Формат не валиден")
+            return None
+    lengths = list()
+    for i in range(len(list_of_lists[0])):
+        current_length = 0
+        for j in list_of_lists:
+            current_length = max(len(str(j[i])), current_length)
+        lengths.append(current_length)
+    print("-" * (sum(lengths) + 4 * len(lengths) + 1))
+    if headers:
+        data_headers = list_of_lists.pop(0)
+        for i, j in enumerate(data_headers):
+            print("|  {:^{width}}  ".format(j, width=lengths[i]), end='')
+        print("|")
+    for i in list_of_lists:
+        for j, k in enumerate(i):
+            output = "|  {:" + (">" if j == len(i) - 1 else "<") + "{width}}  "
+            print(output.format(k, width=lengths[j]), end='')
+        print("|")
+    print("-" * (sum(lengths) + 4 * len(lengths) + 1))
