23b1647a46c0572282b741647a990d97b712a643
Zarina Sayfullina <z.sayfullina@corp.mail.ru>
2019-03-10 20:21:03 +0300

    added test for hw2 table

--- tests/test_hw_02.py
+++ tests/test_hw_02.py
@@ -86,6 +86,7 @@ def test_vk_poster():
                 assert ans == vk_poster.get_most_popular_posts(*params)
     return True
 
+
 def test_table():
     data = load_test_data("table")
 
@@ -95,12 +96,18 @@ def test_table():
             with os.fdopen(fd, 'wb') as tmp:
                 tmp.write(test_data)
 
-            out, err = subprocess.Popen(['python', 'homeworks/homework_02/table.py', path], stdout=subprocess.PIPE).communicate()
+            out, err = subprocess.Popen(
+                ['python', 'homeworks/homework_02/table.py', path],
+                stdout=subprocess.PIPE,
+            ).communicate()
             assert test_out.strip() == out.decode('utf8').strip()
         finally:
             os.remove(path)
 
     # Not Found
-    out, err = subprocess.Popen(['python', 'homeworks/homework_02/table.py', 'path'], stdout=subprocess.PIPE).communicate()
+    out, err = subprocess.Popen(
+        ['python', 'homeworks/homework_02/table.py', 'path'],
+        stdout=subprocess.PIPE,
+    ).communicate()
     assert 'Файл не валиден' == out.decode('utf8').strip()
     return True
