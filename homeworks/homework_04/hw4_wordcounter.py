0175197e01ecfe12e52b070a19e903c8c98bd592
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-03-24 12:04:37 +0300

    Word counter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -2,7 +2,13 @@
 # coding: utf-8
 
 from multiprocessing import Process, Manager
-import os
+from os import listdir
+from collections import deque
+
+
+def _count(path_to_dir, filename, result_dict):
+    with open(path_to_dir + '/' + filename, 'r', encoding='utf8') as f:
+        result_dict.update([(filename, len(f.read().split()))])
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +22,17 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    tasks_list = deque()
+    result_dict = Manager().dict()
+    try:
+        for i in listdir(path_to_dir):
+            task = Process(target=_count, args=(path_to_dir, i, result_dict))
+            tasks_list.append(task)
+            task.start()
+    except FileNotFoundError:
+        return
+    for i in tasks_list:
+        if i.is_alive():
+            i.join()
+    result_dict.update({"total": sum(result_dict.values())})
+    return result_dict
