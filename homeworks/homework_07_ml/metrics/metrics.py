0b034060d4b797ef650811e7e148b02ca63cf2e2
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-04-21 12:48:48 +0300

    Metrics

--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -4,52 +4,78 @@
 
 import numpy as np
 
+THRESHOLD = 0.5
+
 
 def logloss(y_true, y_pred):
     """
     logloss
     :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated probabilities
+    :param y_pred: vector of estimated probabilities
     :return: loss
     """
-    pass
+    return -1 / y_true.shape[0] * \
+           (y_true * np.log(y_pred) +
+            (1 - y_true) * np.log(1 - y_pred)
+            ).sum()
 
 
-def accuracy(y_true, y_pred):
+def accuracy(y_true, y_pred, threshold=THRESHOLD):
     """
     Accuracy
     :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
+    :param y_pred: vector of estimated class values
     :return: loss
     """
-    pass
+    return y_true[((y_true == 1) & (y_pred >= threshold)) | \
+                  ((y_true == 0) & (y_pred < threshold))].shape[0] / \
+           y_true.shape[0]
 
 
-def presicion(y_true, y_pred):
+def precision(y_true, y_pred, threshold=THRESHOLD):
     """
-    presicion
+    precision
     :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
+    :param y_pred: vector of estimated class values
     :return: loss
     """
-    pass
+    return y_pred[(y_pred >= threshold) & (y_true == 1)].shape[0] / \
+           y_pred[y_pred >= threshold].shape[0]
 
 
-def recall(y_true, y_pred):
+def recall(y_true, y_pred, threshold=THRESHOLD):
     """
-    presicion
+    recall
     :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
+    :param y_pred: vector of estimated class values
     :return: loss
     """
-    pass
+    return y_pred[(y_pred >= threshold) & (y_true == 1)].shape[0] / \
+           y_true[y_true == 1].shape[0]
+
+
+def false_recall(y_true, y_pred, threshold=THRESHOLD):
+    """
+        false_recal
+        :param y_true: vector of truth (correct) class values
+        :param y_pred: vector of estimated class values
+        :return: loss
+        """
+    return y_pred[(y_pred >= threshold) & (y_true == 0)].shape[0] / \
+           y_true[y_true == 0].shape[0]
 
 
 def roc_auc(y_true, y_pred):
     """
     roc_auc
     :param y_true: vector of truth (correct) target values
-    :param y_hat: vector of estimated probabilities
+    :param y_pred: vector of estimated probabilities
     :return: loss
     """
-    pass
+    data = dict()
+    for threshold in np.linspace(0, 1):
+        data[false_recall(y_true, y_pred, threshold)] = \
+            recall(y_true, y_pred, threshold)
+    x_data = sorted([i for i in data.keys()])
+    y_data = [data[i] for i in x_data]
+    return np.trapz(y_data, x=x_data)
