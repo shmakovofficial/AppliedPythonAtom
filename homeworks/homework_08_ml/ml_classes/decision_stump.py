2d48e349d0eaddb20bb4c025f3f60b1b6140e75a
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-04-29 01:46:42 +0300

    KNN and Decision stump

--- homeworks/homework_08_ml/ml_classes/decision_stump.py
+++ homeworks/homework_08_ml/ml_classes/decision_stump.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+
 
 class DecisionStumpRegressor:
     '''
@@ -13,7 +15,12 @@ class DecisionStumpRegressor:
         Мы должны создать поля, чтобы сохранять наш порог th и ответы для
         x <= th и x > th
         '''
-        raise NotImplementedError
+        self.le_y = None
+        self.gt_y = None
+        self.threshold = None
+        self.length = None
+        self.fitted = False
+        self.predict_vector = np.vectorize(self.decide)
 
     def fit(self, X, y):
         '''
@@ -22,7 +29,67 @@ class DecisionStumpRegressor:
         :param y: целевая переменная (1, num_objects)
         :return: None
         '''
-        raise NotImplementedError
+        # сортируем значения
+        self.length = X.shape[0]
+        values_id = np.argsort(X)
+        sorted_values = np.vstack((X[values_id], y[values_id]))
+        # три массива для хранения средних значений и ошибок
+        le_means = np.empty(self.length)
+        le_means2 = np.empty(self.length)
+        gt_means = np.empty(self.length)
+        gt_means2 = np.empty(self.length)
+        mse = np.empty(self.length)
+        mse2 = np.empty(self.length)
+        # заполнение начальными значениями
+        le_means[0] = np.nan
+        le_means2[0] = le_means[0]
+        gt_means[0] = y.mean()
+        gt_means2[0] = gt_means[0]
+        mse[0] = ((y - gt_means[0]) ** 2).mean()
+        mse2[0] = mse[0]
+        # заполнение массивов в цикле с пересчетом за константное время
+        for i in range(1, self.length):
+            le_means[i] = self.le_mean_new(
+                le_means[i - 1],
+                i,
+                sorted_values[1][i - 1]
+            )
+            if i == 1:
+                le_means2[1] = sorted_values[1][0]
+            else:
+                le_means2[i] = sorted_values[1][:i].mean()
+            gt_means[i] = self.gt_mean_new(
+                gt_means[i - 1],
+                self.length,
+                i,
+                sorted_values[1][i - 1]
+            )
+            gt_means2[i] = sorted_values[1][i:].mean()
+            mse2[i] = (((sorted_values[1][:i] - le_means[i]) ** 2).sum() +
+                       ((sorted_values[1][i:] - gt_means[i]) ** 2).sum() /
+                       self.length)
+            mse[i] = self.mse_new(
+                mse[i - 1],
+                i,
+                le_means[i - 1],
+                le_means[i],
+                gt_means[i - 1],
+                gt_means[i],
+                self.length
+            )
+
+        # выбор параметров с наименьшей ошибкой
+        min_index = np.argmin(mse)
+        if min_index == 0:
+            self.le_y = le_means[0]
+            self.threshold = sorted_values[0][self.length - 1]
+            self.gt_y = np.nan
+        else:
+            self.le_y = le_means[min_index]
+            self.threshold = (sorted_values[0][min_index - 1] +
+                              sorted_values[0][min_index]) / 2
+            self.gt_y = gt_means[min_index]
+        self.fitted = True
 
     def predict(self, X):
         '''
@@ -30,4 +97,47 @@ class DecisionStumpRegressor:
         :param X: массив размера (1, num_objects)
         :return: массив, размера (1, num_objects)
         '''
-        raise NotImplementedError
+        if not self.fitted:
+            raise ValueError("Model is not fitted")
+        return self.predict_vector(X)
+
+    def decide(self, x):
+        if x <= self.threshold:
+            return self.le_y
+        else:
+            return self.gt_y
+
+    def le_mean_new(self, le_mean_old, index, value):
+        if np.isnan(le_mean_old):
+            return value
+        else:
+            return (le_mean_old * (index - 1) + value) / index
+
+    def gt_mean_new(self, gt_mean_old, length, index, value):
+        return (gt_mean_old * (length - index + 1) - value) / (length - index)
+
+    def mse_new(self, mse_old, index, le_mean_old, le_mean_new,
+                gt_mean_old, gt_mean_new, length):
+        if index == 1:
+            return (mse_old +
+                    (length * (gt_mean_old ** 2) -
+                     le_mean_new ** 2 -
+                     (length - 1) * (gt_mean_new ** 2)) / length)
+        else:
+            return (mse_old +
+                    ((index - 1) * (le_mean_old ** 2) +
+                     (length - index + 1) * (gt_mean_old ** 2) -
+                     index * (le_mean_new ** 2) -
+                     (length - index) * (gt_mean_new ** 2)) / length)
+
+
+if __name__ == "__main__":
+    a = np.array([0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0])
+    b = np.array([i for i in range(16)])
+    np.random.shuffle(b)
+    a = a[b]
+    c = np.array([i for i in range(0, 12, 3)])
+    dst = DecisionStumpRegressor()
+    dst.fit(b, a)
+    print(c)
+    print(dst.predict(c))
--- homeworks/homework_08_ml/ml_classes/knns.py
+++ homeworks/homework_08_ml/ml_classes/knns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+
 
 class KNNRegressor:
     """
@@ -12,34 +14,61 @@ class KNNRegressor:
         Конструктор
         :param n: число ближайших соседей, которые используются
         '''
-        raise NotImplementedError
+        self.n = n
+        self.x = None
+        self.y = None
+        self.fitted = False
 
-    def fit(self, X, y):
+    def fit(self, x, y):
         '''
-        :param X: обучающая выборка, матрица размерности (num_obj, num_features)
+        :param x: обучающая выборка, матрица размерности (num_obj, num_features)
         :param y: целевая переменная, матрица размерности (num_obj, 1)
         :return: None
         '''
-        raise NotImplementedError
+        self.x = x
+        self.y = y
+        self.fitted = True
 
     def predict(self, X):
         '''
         :param X: выборка, на которой хотим строить предсказания (num_test_obj, num_features)
         :return: вектор предсказаний, матрица размерности (num_test_obj, 1)
         '''
-
-        raise NotImplementedError
-
         y = []
         assert len(X.shape) == 2
         for t in X:
             # Посчитаем расстояние от всех элементов в тренировочной выборке
             # до текущего примера -> результат - вектор размерности трейна
-            # TODO d =
+            d = np.linalg.norm(self.x - t, ord=2, axis=1)
             # Возьмем индексы n элементов, расстояние до которых минимально
             # результат -> вектор из n элементов
-            # TODO idx =
-            # TODO
-            prediction = None
-            y.append(prediction)
+            idx = np.argsort(d)[:self.n]
+            if np.isclose(d[idx[0]], 0):
+                y.append(self.y[idx[0]][0])
+            else:
+                prediction = 0
+                inverted_distance = (1 / d[idx]).sum()
+                for i in idx:
+                    prediction += self.y[i][0] / d[i]
+                y.append(prediction / inverted_distance)
         return y
+
+
+if __name__ == "__main__":
+    # y = x * 5
+    # для четного числа соседей mse будет почти нулевой
+    # ввиду симметричности выборок
+    for number in range(1, 10):
+        a = np.empty((50, 1))
+        b = np.empty((50, 1))
+        c = np.empty((50 - number * 2, 1))
+        for i in range(0, 50):
+            a[i][0] = i * 2
+            b[i][0] = (i * 2) * 5
+        knn = KNNRegressor(number)
+        knn.fit(a, b)
+        test = np.empty((50, 1))
+        for i in range(0, 50 - number * 2):
+            c[i][0] = (i + number) * 2 + 1
+        print("For {} neighbors MSE is {}".format(
+            number, ((np.array(knn.predict(c)) - c.T[0] * 5) ** 2).mean()))
